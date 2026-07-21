93ad009a474fa0a0d700fed6cc6a0b6901655d1a
Dima <d@kazhadey.ru>
2026-07-21 14:06:35 +0300

    Добавьте файлы проекта.

---
+++ VTBL.AddressNormalizer.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace VTBL.AddressNormalizer.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
